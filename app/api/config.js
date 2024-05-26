f0d5cb9674441ecc924e4213645221a26cf1b7be
anastassi1a <akor5665@gmail.com>
2024-05-26 18:40:20 +0500

    Правки

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://yourdomain.backend.nomoredomainswork.ru' 
-
+export const BASE_URL = 'https://pindie-backend.nomoredomainswork.ru/api' ;
+ 
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
