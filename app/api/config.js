29d07b196e0f775fe45ce7a3d773c159d015b7ff
anastassi1a <akor5665@gmail.com>
2024-05-26 18:46:05 +0500

    domain

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://pindie-backend.nomoredomainswork.ru/api' ;
- 
+export const BASE_URL = 'https://backend.nomoredomainswork.ru/api' ;
+
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
